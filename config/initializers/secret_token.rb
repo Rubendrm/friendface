eb81dcbd02b7193f68eedac455a184ac4e7bbad6
Ruben Rojas <rubendrojasm@gmail.com>
2013-07-23 17:52:48 -0700

    Created a rails application and added staus updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendface::Application.config.secret_token = '7a8da2a9aa9c5bb36c6150c03a1944aff9c18280b1c3a86eb46863e60c3a4045bbb42514d44116417df4e305f1a6c660e54212f307ed3569821b0cb93055053d'
